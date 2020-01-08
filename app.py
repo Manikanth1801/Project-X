6af38250fc998f974de1f7b54152cd04f6106dd7
Sivananda Panda <sivananda.panda@gmail.com>
2020-01-08 12:03:58 +0530

    Logout page is updated

--- app.py
+++ app.py
@@ -45,6 +45,11 @@ def login_user():
 
     return render_template("profile.html", email=session['email'])
 
+@app.route('/logout')
+def logout_user():
+    User.logout()
+    return render_template('login.html')
+
 
 @app.route('/auth/register', methods=['POST'])
 def register_user():
