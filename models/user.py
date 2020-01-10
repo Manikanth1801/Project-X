18b551981147e80c642b11a20e1bc4b49a9e6cf5
Sreerag  CT <42405365+sreeragct@users.noreply.github.com>
2020-01-10 20:27:23 +0530

    Update User.py
    Added encryption for storing password
    Added condition for logging in using both username or email
    Made some minor modifications in existing function regarding user such as adding new fields

--- models/user.py
+++ models/user.py
@@ -1,58 +1,80 @@
 import datetime
 import uuid
-from flask import session
+from flask import session, flash, render_template
 from common.database import Database
 from models.blog import Blog
+from passlib.hash import sha256_crypt
 
 
 class User(object):
-    def __init__(self, email, password, _id=None):
+    def __init__(self, name, email, username, password, _id=None):
+        self.name = name
         self.email = email
+        self.username = username
         self.password = password
         self._id = uuid.uuid4().hex if _id is None else _id
 
     @classmethod
-    def get_by_email(cls, email):
-        data = Database.find_one("users", {"email": email})
-        if data is not None:
-            return cls(**data)
-
-    @classmethod
-    def get_by_id(cls, _id):
-        data = Database.find_one("users", {"_id": _id})
-        if data is not None:
-            return cls(**data)
-
-    @staticmethod
-    def login_valid(email, password):
-        # Check whether a user's email matches the password they sent us
-        user = User.get_by_email(email)
-        if user is not None:
-            # Check the password
-            return user.password == password
+    def get_by_email(cls, email, username):
+        data1 = Database.find_one("users", {"email": email})
+        data2 = Database.find_one("users", {"username": username})
+        if data1 is not None:
+            return cls(**data1)
+        elif data2 is not None:
+            return cls(**data2)
         return False
 
     @classmethod
-    def register(cls, email, password):
-        user = cls.get_by_email(email)
-        if user is None:
+    def get_by_id(cls, _id):
+        data = Database.find_one("test", {"_id": _id})
+        if data is not None:
+            return cls(**data)
+
+    @staticmethod
+    def login(email, password):
+        # Check whether a user's email matches the password they sent us
+        data1 = Database.find_one("users", {"username": email})
+        data2 = Database.find_one("users", {"email": email})
+        if (data1 or data2) is not None:
+            if data1 is not None:
+                dbPassword = data1['password']
+            else:
+                dbPassword = data2['password']
+            if sha256_crypt.verify(password, dbPassword):
+                session['logged_in'] = True
+                if data1 is not None:
+                    session['username'] = email
+                else:
+                    session['username'] = data2['username']
+                flash('You are now logged in', 'success')
+                return True
+            else:
+                flash('Invalid login')
+                return render_template('login.html')
+        else:
+            flash('Username not found')
+            return render_template('login.html')
+
+    @classmethod
+    def register(cls, name, email, username, password):
+        user = cls.get_by_email(email, username)
+        if user is False:
             # User doesn't exist, so we can create it
-            new_user = cls(email, password)
+            enc_password = sha256_crypt.encrypt(str(password))
+            new_user = cls(name, email, username, enc_password)
             new_user.save_to_mongo()
-            session['email'] = email
+            session['username'] = username
+            session['logged_in'] = True
+            flash('You are now registered', 'success')
             return True
         else:
-            # User exists :(
             return False
 
     @staticmethod
-    def login(user_email):
-        # login_valid has already been called
-        session['email'] = user_email
-
-    @staticmethod
     def logout():
-        session['email'] = None
+        session.clear()
+        flash('You are now logged out', 'success')
+        return True
 
     def get_blogs(self):
         return Blog.find_by_author_id(self._id)
@@ -74,8 +96,10 @@ class User(object):
 
     def json(self):
         return {
-            "email": self.email,
             "_id": self._id,
+            "name": self.name,
+            "email": self.email,
+            "username": self.username,
             "password": self.password
         }
 
