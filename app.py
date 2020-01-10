0bf8b5a8fe79043cd284a4802a3caa8f57cdbcf6
Sreerag  CT <42405365+sreeragct@users.noreply.github.com>
2020-01-10 20:35:02 +0530

    Update app.py
    Added a new method called is logged in to check whether a user has been logged in the system before accessing a partiular function,
    Did some minor updates here and there regarding registration and login

--- app.py
+++ app.py
@@ -2,17 +2,12 @@ from common.database import Database
 from models.blog import Blog
 from models.post import Post
 from models.user import User
+from flask import Flask, render_template, request, session, make_response, redirect, url_for, flash
+from functools import wraps
 
 
-
-
-from flask import Flask, render_template, request, session, make_response, redirect, url_for
-from flask_nav import Nav
-from flask_nav.elements import Navbar, View
-
-app = Flask(__name__)  # '__main__'
-app.secret_key='siva123'
-
+app = Flask(__name__)
+app.secret_key = 'siva123'
 
 
 @app.route('/')
@@ -35,38 +30,58 @@ def initialize_database():
     Database.initialize()
 
 
-@app.route('/auth/login', methods=['POST'])
+@app.route('/auth/login', methods=['POST', 'GET'])
 def login_user():
-    email = request.form['email']
-    password = request.form['password']
-
-    if User.login_valid(email, password):
-        User.login(email)
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        if User.login(email, password):
+            return redirect(url_for('Profile_of_User'))
     else:
-        session['email'] = None
+        return render_template("login.html")
 
-    return render_template("profile.html", email=session['email'])
+
+# Check if user logged in
+def is_logged_in(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('Unauthorized User, Please login')
+            return redirect(url_for('login_user'))
+    return wrap
+
+
+@app.route('/profile')
+@is_logged_in
+def Profile_of_User():
+    return render_template("profile.html")
+
 
 @app.route('/logout')
+@is_logged_in
 def logout_user():
     User.logout()
     return render_template('login.html')
 
 
-@app.route('/auth/register', methods=['POST'])
+@app.route('/auth/register', methods=['POST', 'GET'])
 def register_user():
-    email = request.form['email']
-    password = request.form['password']
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        username = request.form['username']
+        password = request.form['password']
 
-    if User.register(email, password):
-        session['email'] = email
-        return render_template("profile.html", email=session['email'])
-    else:
-        session['email']=None
-        return render_template("login.html")
+        if User.register(name, email, username, password):
+            session['username'] = username
+            return redirect(url_for('Profile_of_User'))
 
-
-   
+        flash('Either Username or Email is already registered in the system!', 'error')
+        # Add a comment saying you are already registered
+        session['username'] = None
+    return redirect(url_for('register_template'))
 
 
 @app.route('/blogs/<string:user_id>')
@@ -121,4 +136,4 @@ def create_new_post(blog_id):
 
 
 if __name__ == '__main__':
-    app.run( debug=True)
+    app.run(debug=True)
