a2e86f07a3c3b7d586da7d707bcfc27c60d28a85
Sreerag  CT <42405365+sreeragct@users.noreply.github.com>
2020-01-13 23:18:31 +0530

    update app.py
    tried adding organizr registration

--- app.py
+++ app.py
@@ -25,6 +25,11 @@ def register_template():
     return render_template('register.html')
 
 
+@app.route('/register/auth/organizer')
+def org_register_template():
+    return render_template('org_register.html')
+
+
 @app.before_first_request
 def initialize_database():
     Database.initialize()
@@ -69,70 +74,85 @@ def logout_user():
 @app.route('/auth/register', methods=['POST', 'GET'])
 def register_user():
     if request.method == 'POST':
-        name = request.form['name']
+        name1 = request.form['name']
         email = request.form['email']
         username = request.form['username']
         password = request.form['password']
+        usertype = request.form['type']
 
-        if User.register(name, email, username, password):
+        if User.register(name1, email, username, password, usertype):
             session['username'] = username
-            return redirect(url_for('Profile_of_User'))
+            data = Database.find_one("test", {"username": session['username']})
+            usrType = data['type']
+            if usrType == 'Organizer':
+                return redirect(url_for('orgReg'))
 
-        flash('Either Username or Email is already registered in the system!', 'danger')
+        flash('Either Username or Email is already registered in the system!', 'error')
         # Add a comment saying you are already registered
         session['username'] = None
     return redirect(url_for('register_template'))
 
 
-@app.route('/blogs/<string:user_id>')
-@app.route('/blogs')
-def user_blogs(user_id=None):
-    if user_id is not None:
-        user = User.get_by_id(user_id)
-    else:
-        user = User.get_by_email(session['email'])
-
-    blogs = user.get_blogs()
-
-    return render_template("user_blogs.html", blogs=blogs, email=user.email)
+@app.route('/auth/register/organizer', methods=['GET', 'POST'])
+def orgReg():
+    if request.method == 'POST':
+        org_name = request.form['org_name']
+        org_email = request.form['org_email']
+        address = request.form['address']
+        if User.orgRegister(org_name, org_email, address):
+            return redirect(url_for('Profile_of_User'))
+    return redirect(url_for('org_register_template'))
 
 
-@app.route('/blogs/new', methods=['POST', 'GET'])
-def create_new_blog():
-    if request.method == 'GET':
-        return render_template('new_blog.html')
-    else:
-        title = request.form['title']
-        description = request.form['description']
-        user = User.get_by_email(session['email'])
-
-        new_blog = Blog(user.email, title, description, user._id)
-        new_blog.save_to_mongo()
-
-        return make_response(user_blogs(user._id))
-
-
-@app.route('/posts/<string:blog_id>')
-def blog_posts(blog_id):
-    blog = Blog.from_mongo(blog_id)
-    posts = blog.get_posts()
-
-    return render_template('posts.html', posts=posts, blog_title=blog.title, blog_id=blog._id)
-
-
-@app.route('/posts/new/<string:blog_id>', methods=['POST', 'GET'])
-def create_new_post(blog_id):
-    if request.method == 'GET':
-        return render_template('new_post.html', blog_id=blog_id)
-    else:
-        title = request.form['title']
-        content = request.form['content']
-        user = User.get_by_email(session['email'])
-
-        new_post = Post(blog_id, title, content, user.email)
-        new_post.save_to_mongo()
-
-        return make_response(blog_posts(blog_id))
+# @app.route('/blogs/<string:user_id>')
+# @app.route('/blogs')
+# def user_blogs(user_id=None):
+#     if user_id is not None:
+#         user = User.get_by_id(user_id)
+#     else:
+#         user = User.get_by_email(session['email'])
+#
+#     blogs = user.get_blogs()
+#
+#     return render_template("user_blogs.html", blogs=blogs, email=user.email)
+#
+#
+# @app.route('/blogs/new', methods=['POST', 'GET'])
+# def create_new_blog():
+#     if request.method == 'GET':
+#         return render_template('new_blog.html')
+#     else:
+#         title = request.form['title']
+#         description = request.form['description']
+#         user = User.get_by_email(session['email'])
+#
+#         new_blog = Blog(user.email, title, description, user._id)
+#         new_blog.save_to_mongo()
+#
+#         return make_response(user_blogs(user._id))
+#
+#
+# @app.route('/posts/<string:blog_id>')
+# def blog_posts(blog_id):
+#     blog = Blog.from_mongo(blog_id)
+#     posts = blog.get_posts()
+#
+#     return render_template('posts.html', posts=posts, blog_title=blog.title, blog_id=blog._id)
+#
+#
+# @app.route('/posts/new/<string:blog_id>', methods=['POST', 'GET'])
+# def create_new_post(blog_id):
+#     if request.method == 'GET':
+#         return render_template('new_post.html', blog_id=blog_id)
+#     else:
+#         title = request.form['title']
+#         content = request.form['content']
+#         user = User.get_by_email(session['email'])
+#
+#         new_post = Post(blog_id, title, content, user.email)
+#         new_post.save_to_mongo()
+#
+#         return make_response(blog_posts(blog_id))
 
 
 if __name__ == '__main__':
