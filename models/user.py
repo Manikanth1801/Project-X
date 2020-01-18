814eed9a61bc69aaa5a0b12ac8ee0f9e68e74628
Sreerag  CT <42405365+sreeragct@users.noreply.github.com>
2020-01-18 20:37:46 +0530

    update user.py

--- models/user.py
+++ models/user.py
@@ -1,23 +1,27 @@
 import datetime
 import uuid
 from flask import session, flash, render_template
-from common.database import Database
-from models.blog import Blog
 from passlib.hash import sha256_crypt
 
+from common.database import Database
+from models.blog import Blog
 
+
+#added usertype
 class User(object):
-    def __init__(self, name, email, username, password, _id=None):
+    def __init__(self, name, email, username, password, usertype, _id=None):
         self.name = name
         self.email = email
         self.username = username
         self.password = password
+        self.usertype = usertype
         self._id = uuid.uuid4().hex if _id is None else _id
 
     @classmethod
     def get_by_email(cls, email, username):
-        data1 = Database.find_one("users", {"email": email})
-        data2 = Database.find_one("users", {"username": username})
+        #check either email or username exits in the database
+        data1 = Database.find_one("test", {"email": email})
+        data2 = Database.find_one("test", {"username": username})
         if data1 is not None:
             return cls(**data1)
         elif data2 is not None:
@@ -26,15 +30,15 @@ class User(object):
 
     @classmethod
     def get_by_id(cls, _id):
-        data = Database.find_one("users", {"_id": _id})
+        data = Database.find_one("test", {"_id": _id})
         if data is not None:
             return cls(**data)
 
     @staticmethod
     def login(email, password):
-        # Check whether a user's email matches the password they sent us
-        data1 = Database.find_one("users", {"username": email})
-        data2 = Database.find_one("users", {"email": email})
+        # Check whether input is either username or password
+        data1 = Database.find_one("test", {"username": email})
+        data2 = Database.find_one("test", {"email": email})
         if (data1 or data2) is not None:
             if data1 is not None:
                 dbPassword = data1['password']
@@ -56,12 +60,12 @@ class User(object):
             return render_template('login.html')
 
     @classmethod
-    def register(cls, name, email, username, password):
+    def register(cls, name, email, username, password, usertype):
         user = cls.get_by_email(email, username)
         if user is False:
             # User doesn't exist, so we can create it
             enc_password = sha256_crypt.encrypt(str(password))
-            new_user = cls(name, email, username, enc_password)
+            new_user = cls(name, email, username, enc_password, usertype)
             new_user.save_to_mongo()
             session['username'] = username
             session['logged_in'] = True
@@ -100,8 +104,69 @@ class User(object):
             "name": self.name,
             "email": self.email,
             "username": self.username,
-            "password": self.password
+            "password": self.password,
+            "type": self.usertype
         }
 
     def save_to_mongo(self):
-        Database.insert("users", self.json()) #use the collection name which is in db always
+        Database.insert("test", self.json())
+
+
+#add following fields in your db for organizer
+class Organizer(object):
+    def __init__(self, org_name, org_email, org_username, address, _id=None):
+        self.org_name = org_name
+        self.org_email = org_email
+        self.org_username = org_username
+        self.address = address
+        self._id = uuid.uuid4().hex if _id is None else _id
+
+    @classmethod
+    def orgRegister(cls, org_name, org_email, org_address):
+        new_organiser = cls(org_name, org_email, session['username'], org_address)
+        new_organiser.save_org_mongo()
+        flash('Organizer details has been added successfully', 'success')
+        return True
+
+    def json_org(self):
+        return {
+            "_id": self._id,
+            "org_name": self.org_name,
+            "org_email": self.org_email,
+            "username": session['username'],
+            "address": self.address
+        }
+
+    def save_org_mongo(self):
+        Database.insert("org", self.json_org())
+
+
+#create participant collection and add following fields
+class Participant(object):
+    def __init__(self, preference1, preference2, preference3, part_username, address, _id=None):
+        self.preference1 = preference1
+        self.preference2 = preference2
+        self.preference3 = preference3
+        self.part_username = part_username
+        self.address = address
+        self._id = uuid.uuid4().hex if _id is None else _id
+
+    @classmethod
+    def partRegister(cls, preference1, preference2, preference3, address):
+        new_participant = cls(preference1, preference2, preference3, session['username'], address)
+        new_participant.save_part_mongo()
+        flash('Participant details has been added successfully', 'success')
+        return True
+
+    def json_part(self):
+        return {
+            "_id": self._id,
+            "preference1": self.preference1,
+            "preference2": self.preference2,
+            "preference3": self.preference3,
+            "username": session['username'],
+            "address": self.address
+        }
+
+    def save_part_mongo(self):
+        Database.insert("part", self.json_part())
