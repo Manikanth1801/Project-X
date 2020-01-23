61d9a1eb8b10fa87706f009f50c14bfeda5aea50
Sivananda Panda <sivananda.panda@gmail.com>
2020-01-24 02:57:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -136,7 +136,7 @@ def partReg():
 
 '''
 It is to design update, delete feature of the user after login. e.g. Updating Password, preferences and all
-@app.route('/account', methods=['GET', 'POST', 'UPDATE', 'DELETE']
+@app.route('/account', methods=['GET', 'POST', 'PUT', 'DELETE']
 def acc_details():
 '''
            
