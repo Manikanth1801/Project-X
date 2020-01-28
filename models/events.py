3d10eb9edf63a041d61a37fe01c1422db8e60b9f
Sivananda Panda <sivananda.panda@gmail.com>
2020-01-28 17:00:10 +0530

    Update events.py

--- models/events.py
+++ models/events.py
@@ -1,11 +1,3 @@
-from blog import Blog
-from post import Post
-#importing multiple classes in user model
-from user import User 
-from participant import Participant
-from organiser import Organizer
-
-
 import datetime
 import uuid
 from flask import session, flash, render_template
@@ -15,8 +7,8 @@ from common.database import Database
 #from models.blog import Blog
 
 class Event(object):
-    def __init__(self, title, description, banner_image, address_line1, address_line2, city, state, country, terms_and_condition, event_category, event_date, event_time, contact_no, email, ticket_price, _id=None):
-        #self.username = username
+    def __init__(self, username, title, description, banner_image, address_line1, address_line2, city, state, country, terms_and_condition, event_category, event_date, event_time, contact_no, email, ticket_price, _id=None):
+        self.username = username
         self.title = title
         self.description = description
         self.banner_image = banner_image
@@ -35,9 +27,9 @@ class Event(object):
         self._id = uuid.uuid4().hex if _id is None else _id
 
     @classmethod
-    def create_event(cls, title, description, banner_image, address_line1, address_line2, city, state, country, terms_and_condition, event_category, event_date, event_time, contact_no, email, ticket_price):
+    def create_event(cls, username, title, description, banner_image, address_line1, address_line2, city, state, country, terms_and_condition, event_category, event_date, event_time, contact_no, email, ticket_price):
     
-        new_event = cls(title, description, banner_image, address_line1, address_line2, city, state, country, terms_and_condition, event_category, event_date, event_time, contact_no, email, ticket_price)
+        new_event = cls(username, title, description, banner_image, address_line1, address_line2, city, state, country, terms_and_condition, event_category, event_date, event_time, contact_no, email, ticket_price)
         new_event.save_event_mongo()
         flash('Event details has been added successfully', 'success')
         return True
@@ -45,22 +37,22 @@ class Event(object):
     def json_event(self):
         return {
             "_id": self._id,
-            #"username" = session['username'],
-            "title" = self.title,
-            "description"= self.description,
-            "banner_image"= self.banner_image,
-            "address_line1"= self.address_line1,
-            "address_line2" = self.address_line2,
-            "city"= self.city,
-            "state"= self.state,
-            "country"= self.country,
-            "terms_and_condition"= self.terms_and_condition,
-            "event_category"= self.event_category,
-            "event_date"= self.event_date,
-            "event_time"= self.event_time,
-            "contact_no"= self.contact_no,
-            "email"= self.email,
-            "ticket_price"= self.ticket_price        
+            "username" : self.username,
+            "title" : self.title,
+            "description" : self.description,
+            "banner_image" : self.banner_image,
+            "address_line1" : self.address_line1,
+            "address_line2" : self.address_line2,
+            "city" : self.city,
+            "state" : self.state,
+            "country" : self.country,
+            "terms_and_condition" : self.terms_and_condition,
+            "event_category" : self.event_category,
+            "event_date" : self.event_date,
+            "event_time" : self.event_time,
+            "contact_no" : self.contact_no,
+            "email" : self.email,
+            "ticket_price" : self.ticket_price        
         }
 
     def save_event_mongo(self):
