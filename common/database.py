ba05eaef167ab98672e56705bc6336a96d8c4eab
Sivananda Panda <sivananda.panda@gmail.com>
2020-02-03 17:13:12 +0530

    update function is written

--- common/database.py
+++ common/database.py
@@ -4,18 +4,17 @@ import pymongo
 class Database(object):
     @staticmethod
     def initialize():
-        #client = pymongo.MongoClient("mongodb+srv://Test:Test123@cluster0-1n9p4.mongodb.net/test?retryWrites=true&w=majority")
+        # client = pymongo.MongoClient("mongodb+srv://Test:Test123@cluster0-1n9p4.mongodb.net/test?retryWrites=true&w=majority")
         client = pymongo.MongoClient("mongodb://test:test123@ds359868.mlab.com:59868/heroku_pt0qk8kw")
         Database.DATABASE = client.get_default_database()
 
     @staticmethod
     def insert(collection, data):
         Database.DATABASE[collection].insert(data)
-        
+
     @staticmethod
     def update(collection, old_val, new_val):
-        Database.DATABASE[collection].update(old_val, {$set:{new_val}})
-
+        Database.DATABASE[collection].update({"username":old_val}, {"$set": {"username":new_val}})
 
     @staticmethod
     def find(collection, query):
@@ -24,3 +23,4 @@ class Database(object):
     @staticmethod
     def find_one(collection, query):
         return Database.DATABASE[collection].find_one(query)
+
